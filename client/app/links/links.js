47770dc65da9fe049fd78560c94260ab175b17cd
Gabs00 <gewen87@gmail.com>
2014-08-11 18:28:16 -0400

    did bower install

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,8 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  //Should have a links property
+  //Should have a data property
+  //Should have getLinks property
+  //should call get links when the controller is loaded <- links.html
 });
