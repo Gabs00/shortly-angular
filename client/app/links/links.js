f7993531a82351ec3212818dd7e52f7ae4a72b01
Gabs00 <gewen87@gmail.com>
2014-08-11 20:10:17 -0400

    fleshing out links, added Links factory

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,7 +3,13 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, Links) {
   // Your code here
   //Should have a links property
+  $scope.data = {};
   //Should have a data property
   //Should have getLinks property
+  console.log('available links ' + Links);
+  console.log('scope ', $scope);
+  $scope.getLinks = function(){
+
+  };
   //should call get links when the controller is loaded <- links.html
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,12 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getLinks = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links/allLinks'
+    });
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
